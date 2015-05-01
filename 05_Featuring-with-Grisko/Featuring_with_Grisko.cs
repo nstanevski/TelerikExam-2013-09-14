b16d0d7076a11abdbf36c1ddfa37425866976672
nstanevski <nikolay.stanevski@gmail.com>
2015-05-01 10:21:52 +0300

    added foreach cycle and brackets

--- 05_Featuring-with-Grisko/Featuring_with_Grisko.cs
+++ 05_Featuring-with-Grisko/Featuring_with_Grisko.cs
@@ -7,7 +7,10 @@ class Featuring_with_Grisko
     static void Main()
     {
         char[] letters = Console.ReadLine().ToCharArray();
-        foreach (char l  in letters)
+        foreach (char l in letters)
+        {
             Console.Write(l);
+        }
+            
     }
 }
